5b37165111ad050d3ef2d74c7182a8bf6f3d3692
evelyn <violetmoonlightmj@gmail.com>
2023-04-20 16:10:35 +0430

    nicknames

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let names = [
+    "Velyxn",
+    "ChrryMllk",
+    "lilamphitrite",
+    "Evelyn"
+]
+
+function getRandomObject(arr) {
+    if (arr.length === 0) {
+      return null; // If the array is empty, return null
+    }
+    const randomIndex = Math.floor(Math.random() * arr.length); // Get a random index of the array
+    return arr[randomIndex]; // Return the object at that index
+  }
+
 let defualt = 20
 function test() {
     let number = document.getElementById("number")
@@ -8,4 +23,9 @@ function test() {
     }
     number.innerText = defualt
     
+}
+
+function nick() {
+    let name = document.getElementById("name")
+    name.innerText = getRandomObject(names)
 }
